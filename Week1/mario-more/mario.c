cc6f44b7ee8eb1beee7e3c11773b91df124ea57d
Matt W <mattw76@live.com.au>
2025-05-16 15:05:49 +1000

    Mario easy and hard mode

---
+++ Week1/mario-more/mario.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int size = 0;
+    do
+    {
+        size = get_int("Enter a number ");
+    }
+    while (size <= 0);
+    int current = size - 1;
+    for (int x = 0; x < size; x++)
+    {
+        for (int y = 0; y < size; y++)
+        {
+            if (y < current)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("#");
+            }
+        }
+        current--;
+        printf("\n");
+    }
+}
